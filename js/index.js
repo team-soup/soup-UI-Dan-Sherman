a6423dbd27b28247497500e07129a03a327a45bc
Dan Sherman <dvsherman@protonmail.com>
2019-02-06 15:47:45 -0700

    added Feature class to index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,21 @@
 class Feature {
-    constructor (element) {
-        this.element = element;
-        
+    constructor (feature) {
+        this.element = feature;
+        this.featureButtons = this.element.querySelector('.buttons');
+        console.log(this.featureButtons);
+        this.featureExpand = feature.querySelector('.expand');
+        this.featureCollapse = feature.querySelector('.collapse');
+        this.featureContent = feature.querySelector('.feature-content p');
+        this.featureButtons.addEventListener('click', () => this.toggleFeature());        
     }
+
+    toggleFeature () {
+        this.featureExpand.classList.toggle('hide-btn');
+        this.featureCollapse.classList.toggle('hide-btn');
+        this.featureContent.classList.toggle('toggle-on');
+    }
+
 }
-
-
-
-
 
 let features = document.querySelectorAll('.feature');
 features.forEach(feature => new Feature(feature));
